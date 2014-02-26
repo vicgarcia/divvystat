6890a6487f9990a6537f2dc7da5e2a64916a0ab5
Vic Garcia <vic.garcia@outlook.com>
2014-02-25 23:50:34 -0600

    refactor javascript for clarity

--- scripts/prime_cache.php
+++ scripts/prime_cache.php
@@ -3,10 +3,9 @@ require_once '../bootstrap.php';
 
 use \Requests;
 
-$url = 'http://74173.ubiquityhosting.com:8000/station';
+$url = 'http://dvmap.chester250.com:8000/station';
 
 $stations = json_decode(Requests::get($url)->body);
-
 foreach ($stations as $station) {
-    $z = Requests::get($url.'/'.$station->id);
+    Requests::get($url.'/'.$station->id);
 }
