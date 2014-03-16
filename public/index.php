792beb82dd5cdc993b950b28048b70043810b250
Vic <vic.garcia@bsctechnology.com>
2014-03-16 01:49:54 -0500

    use DivvyDB alias, optimize db access

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require_once '../bootstrap.php';
 use \Slim;
 use \SlimProject;
 use \MeekroDB;
-use \dChallenge;
+use \dChallenge\DivvyDB as DivvyDB;
 
 $app = new Slim\Slim([
     'view'            => new Slim\Views\Twig,
@@ -33,7 +33,7 @@ $app->get('/', function() use ($app) {
 $app->get('/station', function() use ($app) {
     $stations = array();
     if (($stations = $app->cache->load('stations')) === false) {
-        $stations = (new dChallenge\DivvyDB($app->db))->getStationsData();
+        $stations = (new DivvyDB($app->db))->getStationsData();
         $app->cache->save('stations', $stations, 3600);
     }
     echo json_encode($stations);
@@ -42,21 +42,20 @@ $app->get('/station', function() use ($app) {
 // get station report data from json api (for popup)
 $app->get('/station/:id', function($id) use ($app) {
     if (($stationIds = $app->cache->load('stationIds')) === false) {
-        $stationIds = (new dChallenge\DivvyDB($app->db))->getStationIds();
+        $stationIds = (new DivvyDB($app->db))->getStationIds();
         $app->cache->save('stationIds', $stationIds, 14400);
     }
     if (in_array($id, $stationIds)) {   // check if the station id is valid
-        $db = new dChallenge\DivvyDB($app->db);
         $report = new \stdClass;
 
         if (($timeline = $app->cache->load('timeline_'.$id)) === false) {
-            $timeline = $db->get72HourTimeline($id);
+            $timeline = (new DivvyDB($app->db))->get72HourTimeline($id);
             $app->cache->save('timeline_'.$id, $timeline, 1200);
         }
         $report->timeline = $timeline;
 
         if (($graph = $app->cache->load('graph_'.$id)) === false) {
-            $graph = $db->getDayAveragesGraph($id);
+            $graph = (new DivvyDB($app->db))->getDayAveragesGraph($id);
             $app->cache->save('graph_'.$id, $graph, 14400);
         }
         $report->graph = $graph;
