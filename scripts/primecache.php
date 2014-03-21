6d64165005f700ad4ec05e9aa722eef3bd7a457f
Vic <vic.garcia@bsctechnology.com>
2014-03-20 22:34:11 -0700

    increase timeout

--- scripts/primecache.php
+++ scripts/primecache.php
@@ -5,7 +5,7 @@ use \Requests;
 
 $url = 'http://dvmap.chester250.com/station';
 
-$stations = json_decode(Requests::get($url, [], ['timeout' => 30])->body);
+$stations = json_decode(Requests::get($url, [], ['timeout' => 90])->body);
 foreach ($stations as $station) {
     Requests::get($url.'/'.$station->id);
 }
