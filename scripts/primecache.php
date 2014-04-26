a699fbbe263399fe70324a338a14d8e6afa4c277
Vic <vic.garcia@bsctechnology.com>
2014-04-26 02:18:18 -0500

    add cache clear in prime script

--- scripts/primecache.php
+++ scripts/primecache.php
@@ -2,10 +2,17 @@
 require_once 'bootstrap.php';
 
 use \Requests;
+use \SlimProject;
 
 $url = 'http://dvmap.chester250.com/station';
+$cache = new SlimProject\Cache(new SlimProject\Kv\Redis);
 
+// delete stations cache, make request to reprime and get ids to loop thru
+$cache->delete('stations');
 $stations = json_decode(Requests::get($url)->body);
+
 foreach ($stations as $station) {
+    // delete timeline from cache and reprime
+    $cache->delete('timeline_'.$station->id);
     Requests::get($url.'/'.$station->id, [], ['timeout' => '90']);
 }
