644b0bca598b1fc66a1f03ab7be408d901f3731c
Vic <vic.garcia@bsctechnology.com>
2014-08-26 20:15:13 -0500

    remove unused sql file

--- sql/timeline_data_view.sql
+++
@@ -1,9 +0,0 @@
-create index availabilitys__timestamp__index on availabilitiys (timestamp);
-create view timeline_view as
-    select
-        station_id as 'id',
-        timestamp as 'timestamp',
-        available_bikes as 'bikes'
-    from availabilitys
-    where timestamp > timestampadd(hour, -72, now())
-    order by timestamp desc;
