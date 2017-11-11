0585d53db6f94ca15d3482f21fdb01b043234246
Vic Garcia <vic.garcia@outlook.com>
2017-11-10 23:56:28 -0600

    remove cache prime task no longer used

--- divvystat.php
+++ divvystat.php
@@ -1,44 +1,33 @@
 <?php
 
 /*
- *  usage   : php divvystat.php <option>
- *  options : prime_cache, daily_cache, update_stations, record_data
+ *  php divvystat.php <option>
  *
- *  prime_cache     : perform regular prime of cache for line charts
- *  record_data     : get data from Divvy API and record in app DB
- *  daily_cache     : perform daily cache of day of week charts
+ *  options ---
+ *  record_data : get data from Divvy API and record in app DB
  *  update_stations : update app DB stations table from API
+ *  prune_data : delete data from database older than 45 days
  *
  */
 
 require_once 'bootstrap.php';
 
 
+$db = new DivvyStat\DB(new MeekroDB);
+
 $cli = new Commando\Command();
 
 $cli->option()
     ->require()
     ->must(function ($option) {
-        $options = [
-            'prime_cache', 'record_data', 'daily_cache', 'update_stations', 'prune_data'
-        ];
+        $options = [ 'record_data', 'update_stations', 'prune_data' ];
         return in_array($option, $options);
     })
-    ->describedAs(
-        'cli task : prime_cache, record_data, daily_cache, update_stations, prune_data'
-    );
-
-$db = new DivvyStat\DB(new MeekroDB);
+    ->describedAs('provide a command line option');
 
 switch ($cli[0]) {
-    case 'prime_cache':
-        DivvyStat\Tasks::primeCache($db);
-        break;
     case 'record_data':
         DivvyStat\Tasks::recordData($db);
-        break;
-    case 'daily_cache':
-        DivvyStat\Tasks::dailyCache($db);
         break;
     case 'update_stations':
         DivvyStat\Tasks::updateStations($db);
--- source/DivvyStat/Tasks.php
+++ source/DivvyStat/Tasks.php
@@ -3,6 +3,7 @@ namespace DivvyStat;
 
 class Tasks
 {
+
     public static function updateStations(DB $db)
     {
         $api = new Api;
@@ -41,33 +42,6 @@ class Tasks
                 $availableBikes,
                 $timestamp
             );
-        }
-    }
-
-    public static function primeCache(DB $db)
-    {
-        $url = 'http://divvystat.us/';
-        $cache = new \Kaavii\Cache(\Kaavii\Redis::connect());
-
-        $key = 'stations';
-        $cache->delete($key);
-
-        $stations = $db->getStationsData();
-        $cache->save($key, $stations, 600);
-    }
-
-    public static function dailyCache(DB $divvy)
-    {
-        $url = 'http://divvystat.us/stations';
-        $cache = new \Kaavii\Cache(\Kaavii\Redis::connect());
-
-        $stations = json_decode(\Requests::get($url)->body);
-        foreach ($stations as $station) {
-            $key = 'graph_' . $station->landmark;
-            $cache->delete($key);
-
-            $graph = $divvy->getRecentUsageBar($station->landmark);
-            $cache->save($key, $graph, 86400);
         }
     }
 
