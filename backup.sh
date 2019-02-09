5da03152d0379f60064159df01e41ec3505016a5
Vic Garcia <vic.garcia@outlook.com>
2019-02-09 17:35:13 -0500

    move backup script to deploy path

--- backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# replace the password in the mysqldump command for use in production
-
-# clear the old backup
-rm -rf /opt/divvystat/divvystat.sql.gz
-
-# backup the databases
-mysqldump -u divvystat -ppassword divvystat | gzip > /opt/divvystat/divvystat.sql.gz
---
+++ deploy/backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# replace the PASSWORD in the mysqldump command for use in production
+
+# clear the old backup
+rm -rf /opt/divvystat/public/divvystat.sql.gz
+
+# backup the databases
+mysqldump -u divvystat -pPASSWORD divvystat | gzip > /opt/divvystat/public/divvystat.sql.gz
