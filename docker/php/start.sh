47d375a3c6168b38276b29ac2fc9a497f7781e29
Vic Garcia <vic.garcia@outlook.com>
2019-02-09 18:11:55 -0500

    use php7.1 and improve docker environment

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -2,10 +2,20 @@
 
 # wait for mysql to start before starting the php dev server
 until mysqladmin -h mysql -u root -proot ping &> /dev/null; do
-    echo "waiting for mysql to start ..." && sleep 10
+    echo "waiting for mysql to start" && sleep 30
 done
 echo "starting php development server"
 
-# start php cli dev server
 cd /code
+
+# install php dependencies via composer
+composer install
+
+# install grunt dependencies via npm
+npm install
+
+# build the javascript (rebuild during development w/ docker script and grunt watch)
+# grunt build
+
+# run the php dev server
 php -S 0.0.0.0:8000 -t /code/public
