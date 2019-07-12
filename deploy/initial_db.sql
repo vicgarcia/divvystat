a5624773babd598d42ef8e2a920ad5533811034c
Vic Garcia <vic.garcia@outlook.com>
2019-07-12 16:44:59 -0400

    revise db structure and init script

--- deploy/initial_db.sql
+++ deploy/initial_db.sql
@@ -1,30 +1,29 @@
--- drop the existing database :
--- mysql -u <user> -p<pass> -e 'drop database divvystat'
+-- run this script to re-populate db structure :
+-- mysql -u <user> -p<pass> < initial_db.sql
 
--- create the database :
--- mysql -u <user> -p<pass> -e 'create database divvystat'
+DROP DATABASE divvystat;
 
--- run this script to populate db structure :
--- mysql -u <user> -p<pass> <db name> < initial_db.sql
+CREATE DATABASE divvystat;
+
+USE divvystat;
 
 DROP TABLE IF EXISTS stations;
 CREATE TABLE stations (
-      landmark char(10) not null,
+      terminal char(20) not null,
       name text not null,
       latitude decimal(14,10) not null,
       longitude decimal(14,10) not null,
-      PRIMARY KEY stations__landmark__index (landmark)
+      PRIMARY KEY stations__terminal__index (terminal)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS availabilitys;
 CREATE TABLE availabilitys (
       id int(11) unsigned not null AUTO_INCREMENT,
       timestamp timestamp not null default CURRENT_TIMESTAMP,
-      landmark char(10) not null,
-      status_key int(3) unsigned not null,
-      total_docks int(3) unsigned not null,
-      available_bikes int(3) unsigned not null,
+      terminal char(20) not null,
+      docks int(3) unsigned not null,
+      bikes int(3) unsigned not null,
       PRIMARY KEY (id),
-      KEY availabilitys__landmark__index (landmark),
+      KEY availabilitys__terminal__index (terminal),
       KEY availabilitys__timestamp__index (timestamp)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
