27c153f5bf98e2479f5bcf627b2fe762adf4d519
Vic Garcia <vic.garcia@outlook.com>
2019-10-06 16:08:20 -0400

    revise db to store int station ids

--- deploy/initial_db.sql
+++ deploy/initial_db.sql
@@ -9,21 +9,21 @@ USE divvystat;
 
 DROP TABLE IF EXISTS stations;
 CREATE TABLE stations (
-      terminal char(20) not null,
+      id int(11) unsigned not null,
       name text not null,
       latitude decimal(14,10) not null,
       longitude decimal(14,10) not null,
-      PRIMARY KEY stations__terminal__index (terminal)
+      PRIMARY KEY stations__id__index (terminal)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS availabilitys;
 CREATE TABLE availabilitys (
       id int(11) unsigned not null AUTO_INCREMENT,
       timestamp timestamp not null default CURRENT_TIMESTAMP,
-      terminal char(20) not null,
+      station int(11) unsigned not null,
       docks int(3) unsigned not null,
       bikes int(3) unsigned not null,
       PRIMARY KEY (id),
-      KEY availabilitys__terminal__index (terminal),
+      KEY availabilitys__station__index (station),
       KEY availabilitys__timestamp__index (timestamp)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
